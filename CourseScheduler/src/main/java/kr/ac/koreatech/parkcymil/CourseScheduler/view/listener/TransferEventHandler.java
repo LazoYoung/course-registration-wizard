1cc59f8543227b4c3b6c0b70736b818bca30f0bb
ideal <ideal@211.57.217.248>
2022-11-12 18:07:58 +0900

    Fix #2 BrowserPanel picking wrong item with RowFilter being used

--- CourseScheduler/src/main/java/kr/ac/koreatech/parkcymil/CourseScheduler/view/listener/TransferEventHandler.java
+++ CourseScheduler/src/main/java/kr/ac/koreatech/parkcymil/CourseScheduler/view/listener/TransferEventHandler.java
@@ -4,6 +4,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JTable;
+import javax.swing.RowSorter;
 
 import kr.ac.koreatech.parkcymil.CourseScheduler.entity.Course;
 import kr.ac.koreatech.parkcymil.CourseScheduler.view.component.TimeTableModel;
@@ -22,17 +23,12 @@ public abstract class TransferEventHandler {
 		return new MouseAdapter() {
 			@Override
 			public final void mousePressed(MouseEvent e) {
-				if (e.getButton() != MouseEvent.BUTTON1)
-					return;
-				
-				int index = table.getSelectedRow();
-				
-				if (index > -1)
-					onTransfer(model.getItem(index));
+				if (e.getButton() == MouseEvent.BUTTON1)
+					execute();
 			}
 		};
 	}
-	
+
 	public MouseAdapter getDoubleClickListener() {
 		return new MouseAdapter() {
 			@Override
@@ -40,14 +36,27 @@ public abstract class TransferEventHandler {
 				int btn = e.getButton();
 				int count = e.getClickCount();
 				
-				if (btn == MouseEvent.BUTTON1 && count > 1) {
-					int row = table.getSelectedRow();
-					onTransfer(model.getItem(row));
-				}
+				if (btn == MouseEvent.BUTTON1 && count > 1)
+					execute();
 			}
 		};
 	}
 	
 	public abstract void onTransfer(Course c);
 
+	@SuppressWarnings("unchecked")
+	private void execute() {
+		int row = table.getSelectedRow();
+		
+		if (row > -1) {
+			RowSorter<TimeTableModel> rs = (RowSorter<TimeTableModel>) table.getRowSorter();
+			int index = row;
+			
+			if (rs != null)
+				index = rs.convertRowIndexToModel(row);
+			
+			onTransfer(model.getItem(index));
+		}
+	}
+	
 }
